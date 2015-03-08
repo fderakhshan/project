8008130012162cb67b952fbc7d39a2e241814911
fderakhshan <ffderakhshan@csupomona.edu>
2015-03-07 22:45:47 -0800

    Create ServletLifecycleExample.java

---
+++ ServletLifecycleExample.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+
+public class ServletLifecycleExample extends GenericServlet {
+    
+    @Override
+    public void init() {
+        // initialize the servlet, and print something in the console.
+        System.out.println("Servlet Initialized!");
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response)
+            throws ServletException, IOException {
+        
+        // the service method will 
+        response.setContentType("phantomjs github.js");
+        response.setContentType("IMAGE.Png");
+        reponse.end("Webhook URL");
+        PrintWriter out = response.getWriter();
+        out.println("Servlet called from jsp page!");
+    }
+    
+    @Override
+    public void destroy() {
+        // close connections etc.
+    }
+}
