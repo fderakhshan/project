b8d8507a3d3687004b1fa6f2dff8eae96065ce60
fderakhshan <ffderakhshan@csupomona.edu>
2015-03-08 10:33:12 -0700

    Update github.js

--- github.js
+++ github.js
@@ -1,5 +1,5 @@
 var page = require('webpage').create();
-page.open('http://github.com/', function() {
+page.open('https://github.com/fderakhshan/project/blob/master/index.jsp/', function() {
   page.render('github.png');
   phantom.exit();
 });
